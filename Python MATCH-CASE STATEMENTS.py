21fdab126452e1a196d3845be95e6385df57208b
Mahesh Perera <165042259+maheshkmp@users.noreply.github.com>
2024-07-28 12:45:46 +0530

    third commit

--- Python MATCH-CASE STATEMENTS.py
+++ Python MATCH-CASE STATEMENTS.py
@@ -2,23 +2,23 @@
 #                               Execute some code if a value matches a 'case
 #                               Benefits: cleaner and syntax is more readable
 
-def day_of_week(day):
+def is_weekend(day):
     match day:
-        case 1:
-            return "It is Sunday"
-        case 2:
-            return "It is Monday"
-        case 3:
-            return "It is Tuesday"
-        case 4:
-            return "It is Wednesday"
-        case 5:
-            return "It is Thursday"
-        case 6:
-            return "It is Friday"
-        case 7:
-            return "It is Saturday"
+        case "Sunday":
+            return True
+        case "Monday":
+            return False
+        case "Tuesday":
+            return False
+        case "Wednesday":
+            return False
+        case "Thursday":
+            return False
+        case "Friday":
+            return False
+        case "Saturday":
+            return True
         case _:
             return "Not a valid day"
         
-print(day_of_week(3))
+print(is_weekend("Sunday"))
